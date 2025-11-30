cc0d28ac8d9431f2a7131aa0c5382afcbb905f48
Arwaf8 <Arwafahad86@gmail.com>
2025-11-30 11:33:23 +0300

    did profile branch, no code yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MainLayout from "./Layouts/MainLayout";
+import MainLayout from "./layouts/MainLayout";
 import Home from "./modules/home/layouts/Home";
 
 export default function App() {
